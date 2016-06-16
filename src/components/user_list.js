1a734439662841023177e4e4ed91a0b6991a7c95
riccardo lorenzon <riccardo.lorenzon@gmail.com>
2016-06-16 17:31:08 +0200

    middleware

---
+++ src/components/user_list.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+
+class UserList extends Component {
+  componentWillMount() {
+    this.props.fetchUsers();
+  }
+
+  renderUser(user) {
+    return (
+      <div className="card card-block">
+        <h4 className="card-title">
+          {user.name}
+        </h4>
+        <p className="card-text">
+          {user.company.name}
+        </p>
+        <a className="btn btn-primary" href={user.website}>
+          Website
+        </a>
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        {this.props.users.map(this.renderUser)}
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state, actions) {
+  return { users: state.users };
+}
+
+export default connect(mapStateToProps, actions)(UserList);
--- src/index.js
+++ src/index.js
@@ -5,8 +5,10 @@ import { createStore, applyMiddleware } from 'redux';
 
 import App from './components/app';
 import reducers from './reducers';
+import Async from './middlewares/async';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+// There can be a chain of middlewares like Async, Logger, etc.
+const createStoreWithMiddleware = applyMiddleware(Async)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
