bb5fa887e346971ec78aaea094f8675904d00783
riccardo lorenzon <riccardo.lorenzon@gmail.com>
2016-07-11 15:27:17 +0200

    Signup component

--- client/src/components/auth/signup.js
+++ client/src/components/auth/signup.js
@@ -8,6 +8,16 @@ class Signup extends Component {
     this.props.signupUser(formProps);
   }
 
+  renderAlert() {
+    if (this.props.errorMessage) {
+      return (
+        <div className="alert alert-danger">
+          <strong>Oops</strong> {this.props.errorMessage}
+        </div>
+      );
+    }
+  }
+
   render() {
     const { handleSubmit, fields: {email, password, passwordConfirm}} = this.props;
 
@@ -28,6 +38,7 @@ class Signup extends Component {
           <input type='password' className="form-control" {...passwordConfirm} />
           {passwordConfirm.touched && passwordConfirm.error && <div className="error">{passwordConfirm.error}</div>}
         </fieldset>
+        {this.renderAlert()}
         <button action="submit" className="btn btn-primary">Sign up</button>
       </form>
     );
@@ -55,8 +66,12 @@ function validate(values) {
   return errors;
 }
 
+function mapStateToProps(state) {
+  return { errorMessage: state.auth.error };
+}
+
 export default reduxForm({
   form: 'signup',
   fields: ['email', 'password', 'passwordConfirm'],
   validate: validate
-})(Signup);
+}, mapStateToProps, actions)(Signup);
